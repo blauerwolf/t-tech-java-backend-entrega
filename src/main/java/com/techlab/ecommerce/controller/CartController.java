11db8856bc8886ca92a74ab42dc838de4eab40a7
Ernesto Ardenghi <eardenghi@gmail.com>
2025-12-02 21:05:02 -0300

    feat: nuevas entradas para swagger

--- src/main/java/com/techlab/ecommerce/controller/CartController.java
+++ src/main/java/com/techlab/ecommerce/controller/CartController.java
@@ -5,6 +5,7 @@ import com.techlab.ecommerce.dto.response.CartResponseDTO;
 import com.techlab.ecommerce.entity.Cart;
 import com.techlab.ecommerce.repository.CartRepository;
 import com.techlab.ecommerce.service.CartService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,12 +19,14 @@ public class CartController {
     private final CartRepository cartRepo;
 
     @PostMapping("")
+    @Operation(summary = "Crear carrito de compras", description = "Crea un nuevo carrito de compras vacío con saldo total 0.0")
     public CartResponseDTO createCart() {
         Cart cart = cartRepo.save(new Cart());
         return service.getCart(cart.getId());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obtener carrito por ID", description = "Obtiene el carrito de compras a partir de su ID")
     public CartResponseDTO getCart(@PathVariable Long id) {
         return service.getCart(id);
     }
@@ -34,6 +37,7 @@ public class CartController {
     }
 
     @PutMapping("/{id}/items/{itemId}")
+    @Operation(summary = "Agregar ítem", description = "Agrega un producto al carrito de compras")
     public CartResponseDTO updateItem(
             @PathVariable Long id,
             @PathVariable Long itemId,
@@ -42,6 +46,7 @@ public class CartController {
     }
 
     @DeleteMapping("/{id}/items/{itemId}")
+    @Operation(summary = "Eliminar ítem", description = "Elimina un producto del carrito de compras")
     public CartResponseDTO deleteItem(
             @PathVariable Long id,
             @PathVariable Long itemId) {
--- src/main/java/com/techlab/ecommerce/controller/CategoryController.java
+++ src/main/java/com/techlab/ecommerce/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.techlab.ecommerce.controller;
 
 import com.techlab.ecommerce.entity.Category;
 import com.techlab.ecommerce.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +17,13 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @PostMapping
+    @Operation(summary = "Crear categoría", description = "Crea una nueva categoría para productos")
     public Category create(@RequestBody @Valid Category c) {
         return categoryService.create(c);
     }
 
     @GetMapping
+    @Operation(summary ="Listar categorías", description = "Lista todas las categorías en el sistema")
     public List<Category> list() {
         return categoryService.list();
     }
